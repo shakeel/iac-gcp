24907d22d9c39978398304cd3df5f245208ef8e6
Shakeel Mahate <shakeel.mahate@gmail.com>
2024-09-30 10:09:24 -0400

    Add backends for tfstate management

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,15 @@ terraform {
       version = "6.4.0"
     }
   }
+
+  # Specify backend for tfstate
+  backend "local" {
+    path = "terraform/state/terraform.tfstate"
+  }
+
+  # This is how to specify an already existing Google Cloud Storage bucket for tfstate
+  # backend "gcs" {
+  #  bucket = "#your-bucket-name"
+  #  prefix = "terraform/state"
+  # }
 }
